d55d9c8948225ab3dc2057512093c77a60b71297
Aleskandro <aleskandro@hexaflake.zion.lan>
2018-01-05 17:16:00 +0100

    Deleted redundant code: created utils.r

--- src/streaming/reducer.r
+++ src/streaming/reducer.r
@@ -1,39 +1,6 @@
 #!/usr/bin/env Rscript
 # The  reducer
-
-toSecs <- function(ts) {
-    ts <- as.numeric(ts)
-    return (ts[1] * 3600 + ts[2] * 60 + ts[3])
-}
-
-# return a string without leading or trailing spaces
-strip   <- function(str)  gsub("^\\s+|\\s+$", "", str)
-
-# split line into a list
-splitString <- function(line, str) unlist(strsplit(line, str))
-
-# return the IP in a string formatted as x.y.z.w.port
-getIp <- function(str) paste(splitString(str, "\\.")[1:4], collapse = ".")
-
-# an helper to populate the stats lists
-addValueHelper <- function(key, value, lst) {
-    if (is.null(lst[[key]])) {
-        lst[[key]] = value
-    } else {
-        lst[[key]] = lst[[key]] + value
-    }
-    return(lst)
-}
-
-topTenSorted <- function(toSort, message) {
-    i <- 0
-    cat(message, '\n')
-    ordered <- toSort[order(unlist(toSort), decreasing=TRUE)][1:10]
-    invisible(sapply(names(ordered), function(n) {
-        if (!is.na(n))
-            cat(n, ordered[[n]], "\n", sep="\t")
-    }))
-}
+modules::import('utils', attach = TRUE)
 
 # Global variables
 topTenClientsByServers <<- list()
@@ -94,5 +61,7 @@ main <- function() {
     close(con)
     printStats()
 }
+
+if (is.null(modules::module_name()))
     main()
 
